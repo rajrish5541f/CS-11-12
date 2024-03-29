aefc1bd87278db8fc31573e1ca98ca3f818e5921
rajrish5541f <rajrish5541f@gmail.com>
2024-03-29 11:44:50 +0530

    cl4

--- Python/Question prac.py
+++ Python/Question prac.py
@@ -8,18 +8,37 @@
 
 '''
 
-import random
+# import random
+
+# rnge = 5
+# for i in range(1, rnge+1, 2):
+#     print('  '*int(((rnge-i)/2))+(i*'* '))
+# for j in range(rnge-2, 0, -2):
+#     print('  '*int(((rnge-j)/2))+(j*'* '))
 
 
 
-rnge = 5
-for i in range(1, rnge+1, 2):
-    print('  '*int(((rnge-i)/2))+(i*'* '))
-for j in range(rnge-2, 0, -2):
-    print('  '*int(((rnge-j)/2))+(j*'* '))
+# write a program to input time in seconds and output in minutes and seconds
+
+# t = input("Enter time in seconds here: ")
+# t = int(t)
+# minutes = t // 60
+# seconds = t % 60
+# print('The time is :', minutes, 'minutes', seconds, 'seconds.')
 
 
-
-
-
-
+st = input('Enter string : ')
+u=l=a=d=0
+for ch in st:
+    if ch.isupper() == True :
+      u+=1
+    elif ch.islower() == True :
+      l +=1
+    if ch.isalpha() == True :
+      a+=1
+    if ch.isdigit() == True :
+      d += 1
+print('Number of uppercase letters :', u)
+print('Number of lowercase letters :', l)
+print('Number of alphabets :', a)
+print('Number of digits :', d)
