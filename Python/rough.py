10902f399af98009238fe087f0fe7c4d35e44ddd
Raj Rishi <rajrish5541f@gmail.com>
2024-07-11 19:36:47 +0530

    linux commands

--- Python/rough.py
+++ Python/rough.py
@@ -1,17 +1,4 @@
-import pickle       # we'll use it to manage .dat files
-
-fileobj = open(r".\Python\Resources\FH.dat", 'r+b')
-#Inputing data
-inp = ['no', 'yes', 'nope', 'yeah']
-
-#Pickling   : storing data
-if inp!='':
-    pickle.dump(inp, fileobj)
-
-#Unpickling : reading data
-fileobj.seek(0)
-print(pickle.load(fileobj))
-
-fileobj.close()
-# print(data)
-
+f= open(r"./Python/Resources/FH.txt")
+r = f.read(3)
+print(r)
+print(f.readlines())
